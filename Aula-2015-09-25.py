958fc5fa4415635dab68efbf0d8338cfe01216be
cauanicastro <cauani.castro@hotmail.com>
2015-09-26 11:10:40 -0300

    finished up the second exercise, all set

--- Aula-2015-09-25.py
+++ Aula-2015-09-25.py
@@ -1,8 +1,9 @@
 def main():
-    #print(ReOrder()) #program 1
+#uncomment the program you wish to use
+#    print(ReOrder()) #program 1
     print(cofCheck()) #program 2
 
-# Program 1 - Reordering 3 variables without using any auxiliary variable
+# Program 1 - Reordering 3 variables without using any auxiliary variable or any kind of loop
 def ReOrder():
     print("Este programa ira ordenar 3 variaveis\n")
     a = float(input("Entre com o primeiro numero\n"))
@@ -24,23 +25,29 @@ def ReOrder():
 
 # Program 2 - Read 3 floats that will represent the coefficients of a second degree equation
 def cofCheck():
+    print("Este programa ira montar e resolver uma equacao de segundo ou primeiro grau, no formato: ax^2 + bx + c.\n")
     a = float(input("Entre com o valor de a\n"))
     b = float(input("Entre com o valor de b\n"))
     c = float(input("Entre com o valor de c\n"))
     if a == 0:
-        return "Os valores inseridos nao formam uma eq. de segundo grau"
+        print("Os valores inseridos nao formam uma eq. de segundo grau.\n")
+        if b == 0:
+            print("Os valores inseridos nao formam uma eq. de primeiro grau.\n")
+            return "Nenhuma eq. foi encontrada, retornando o valor de c.\nc = %.2f" % c
+        else:
+            x = -c / b
+            return "Eq. de primeiro grau encontrada.\nx = %.2f" % x
     else:
         delta = (b ** 2) - ((4 * a) * c)
-        print("delta -> %f" % delta)
         if delta >= 0:
-            x1 = (-b + (delta ** (1/2))) / 2 * a
-            x2 = (-b - (delta ** (1/2))) / 2 * a
-            return "Raiz real encontrada.1" \
-                   "\nx' = %f\nx'' = %f " % (x1, x2)
+            root = delta ** (1/2.0)
+            x1 = (-b + root) / (2*a)
+            x2 = (-b - root) / (2*a)
+            return "Raiz real encontrada.\nx'  = %.2f\nx'' = %.2f " % (x1, x2)
         else:
             m = (b*-1)/(2*a)
-            n = ((delta*-1) ** (1/2))
-            x1 = "%f + %fni" % (m, n)
-            x2 = "%f - %fni" % (m, ((n**2)**(1/2)))
-            return "Raiz complexa encontrada, retornando a representacao retangular dela.\nx' = %s\nx''= %s" % (x1, x2)
+            n = ((delta*-1) ** (1/2.0))
+            x1 = "%.2f + %.2fni" % (m, n)
+            x2 = "%.2f - %.2fni" % (m, ((n**2)**(1/2.0)))
+            return "Raiz complexa encontrada, retornando a representacao retangular dela.\nx'  = %s\nx'' = %s" % (x1, x2)
 main()
